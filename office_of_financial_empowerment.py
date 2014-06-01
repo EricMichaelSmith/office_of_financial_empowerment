3c7f8b60cf96e5135fbef867cd62431131ea3fff
EricMichaelSmith <ericmsmith@gmail.com>
2014-05-31 21:42:42 -0400

    SQL fixes

--- office_of_financial_empowerment.py
+++ office_of_financial_empowerment.py
@@ -21,6 +21,9 @@ def main():
     """
     Loads the original MySQL Office of Financial Empowerment database and merges the counselor's form
     """
+
+    # Add counselor form input
+    counselor_table_name = counselor_form_input()    
     
     # Pull data from original database
     con = MySQLdb.connect(user='root',
@@ -28,24 +31,25 @@ def main():
                           db='ofe')
     cur = con.cursor()
     cur.execute('SELECT last_name, first_name, ssn, language_spoken, email, phone1, phone2, banking, credit, debt, savings FROM clients')
-    rows = cur.fetchall()    
-    
-    # Get counselor form input
-    counselor_table_name = counselor_form_input()
+    rows = cur.fetchall()
     
     # Join databases
     mysql_s = """
+        CREATE TABLE clients_expanded
         SELECT *
-        FROM clients FULL OUTER JOIN {counselor_table_name}
-        ON clients.ssn = {counselor_table_name}.ssn;
+        FROM clients LEFT JOIN {counselor_table_name}
+        ON clients.ssn = {counselor_table_name}.form_ssn;
         """
     cur.execute(mysql_s.format(counselor_table_name=counselor_table_name))
     
     # Output results
-    cur.execute('SELECT * FROM {table_name}'.format(table_name=counselor_table_name))
+    cur.execute('SELECT * FROM clients_expanded'.format(table_name=counselor_table_name))
     rows = cur.fetchall()
     for row in rows:
         print(row)
+        
+    con.commit()
+    con.close()
         
     
     
@@ -63,7 +67,7 @@ def counselor_form_input():
     mysql_s = """
         CREATE TABLE {table_name}
         (
-        ssn char(9) DEFAULT NULL,
+        form_ssn char(9) DEFAULT NULL,
         next_meeting_date date DEFAULT NULL,
         office_address char(75) DEFAULT NULL,
         grow_personal_income boolean DEFAULT NULL,
@@ -71,15 +75,19 @@ def counselor_form_input():
         bank_savings_per_month float(8, 2) DEFAULT NULL,
         deposit_goal float(8, 2) DEFAULT NULL,
         current_bank_name char(50) DEFAULT NULL,
-        PRIMARY KEY (ssn)
+        PRIMARY KEY (form_ssn)
         );
         """
     cur.execute(mysql_s.format(table_name=table_name))
     mysql_s = """
-        INSERT INTO {table_name} (ssn, next_meeting_date, office_address, grow_personal_income, bank_to_use, bank_savings_per_month, deposit_goal, current_bank_name)
+        INSERT INTO {table_name} (form_ssn, next_meeting_date, office_address, grow_personal_income, bank_to_use, bank_savings_per_month, deposit_goal, current_bank_name)
         VALUES ('123456789', '2014-06-04', '123 Office Place, Brooklyn, NY 12345', 1, NULL, NULL, 100, 'Bank Of America'),
                ('987654321', '2014-06-12', '123 Office Place, Brooklyn, NY 12345', 0, NULL, NULL, NULL, NULL),
-               ('271828182', '2014-06-23', '321 Building Boulevard, Queens, NY 54321', 1, 'Citibank', 75, 50, 'Citibank')
+               ('271828182', '2014-06-23', '321 Building Boulevard, Queens, NY 54321', 1, 'Citibank', 75, 50, 'Citibank');
         """        
+    cur.execute(mysql_s.format(table_name=table_name))
+    
+    con.commit()
+    con.close()
     
     return table_name
